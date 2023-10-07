a320d9731c2aa8936dcf244efccb76171df333fb
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-10-07 12:14:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LABB5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            circle1.getArea();
+            Circle circle2 = new Circle(6);
+            circle2.getArea();
+        }
+        public class Circle
+        { 
+            float _pi = 3.141f;
+            int _Radius = 0;
+            
+            public Circle(int radius)
+            {
+                _Radius = radius;
+            }
+            public float getArea()
+            {
+                float area = _Radius * _Radius * _pi;
+                Console.WriteLine($"Arean för cirkel med {_Radius} i radie är: {area}");
+                return area;
+            }
+        }
+    }
+}
