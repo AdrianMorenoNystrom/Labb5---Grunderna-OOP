9c584cf331e885bf05245877c944b4f880a9c31e
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-10-09 15:57:56 +0200

    .

---
+++ Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABB5
+{
+    public class Circle
+    {
+        float _pi = 3.141f;
+        int _Radius = 0;
+
+        public Circle(int radius)
+        {
+            _Radius = radius;
+        }
+        public float getArea()
+        {
+            float area = _Radius * _Radius * _pi;
+            Console.WriteLine($"Arean för cirkel med {_Radius} i radie är: {area}");
+            return area;
+        }
+    }
+}
