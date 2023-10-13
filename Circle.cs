cade22b1e76c790fe140d58f4016a2f922d336eb
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-10-13 11:50:30 +0200

    kommentarer

--- Circle.cs
+++ Circle.cs
@@ -8,13 +8,18 @@ namespace LABB5
 {
     public class Circle
     {
+        //Fält-Variabler. här sätter jag _pi till 3.141 och _Radius till 0, radius kommer att ändras senare.
         float _pi = 3.141f;
         int _Radius = 0;
 
+        //Konstruktor med Radius
         public Circle(int radius)
         {
             _Radius = radius;
         }
+
+        //Metod som skapar variabeln area och räknar ut arean.
+        //samt skriver ut svaret i konsolen och retunerar area.
         public float getArea()
         {
             float area = _Radius * _Radius * _pi;
--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
+            //Här skapar jag 2 nya instanser av Circle som jag döper till circle1 och circle2. 
+            //konstruktorn tar 5 och 6 som argument och sparar sifforna till uträkningen.
             Circle circle1 = new Circle(5);
             Circle circle2 = new Circle(6);
+            //Här används metoden där uträkningen sker samt utskrift. Med argumenten som skickades med när jag skapade instanserna.
             circle1.getArea();
             circle2.getArea();
         }
